0064d4c7ae4b656c4926fb7fc8be67bc0a47c4d7
Emilio Martinez Rivero <emiliomartinezriv@gmail.com>
2022-06-08 21:58:39 +0200

    corrección de error en la url

--- src/config/keycloak-admin.ts
+++ src/config/keycloak-admin.ts
@@ -3,5 +3,5 @@ import Keycloak from "keycloak-js";
 export default Keycloak({
   clientId: "drakars-admin-api",
   realm: "drakars-admin",
-  url: `${process.env.REACT_APP_API_URL}/auth`,
+  url: `${process.env.REACT_APP_AUTH_URL}`,
 });
--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -3,5 +3,5 @@ import Keycloak from "keycloak-js";
 export default Keycloak({
   clientId: "drakcars-api",
   realm: "drakcars",
-  url: "http://keycloak:8080/auth",
+  url: `${process.env.REACT_APP_AUTH_URL}`,
 });
